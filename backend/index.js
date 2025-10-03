bb0d9d9c7038daed89f7c1b3b32aa16d081ef340
Diego Cortez <diego.fernando1399@gmail.com>
2025-10-02 21:45:33 -0600

    Coleccion de postman

--- backend/index.js
+++ backend/index.js
@@ -58,7 +58,6 @@ app.post('/register', async (req, res) => {
 
 app.post('/login', async (req, res) => {
     const {username, password} = req.body
-
     try {
 
         const [user] = await db.promise().query('SELECT * FROM usuarios WHERE username = ?', [username]);
@@ -80,5 +79,40 @@ app.post('/login', async (req, res) => {
     } catch (error) {
         console.error('Error en el login ', error.message);
         res.status(500).json({message: 'Error en el login'})
+    }
+})
+
+app.get('/cuenta', async (req, res) => {
+    const {userid} = req.body;
+    
+    try {
+        const [cuentas] = await db.promise().query('SELECT * FROM cuenta WHERE usuario_id = ?', [userid]);
+
+        return res.status(200).json({cuentas: cuentas})
+
+    } catch(error) {
+        console.error('Error al obtener cuentas ', error.message);
+        res.status(500).json({message: 'Error al obtener cuentas'})
+    }
+})
+
+app.post('/new_cuenta', async (req, res) => {
+    const {usuario, numero, saldo, tipo} = req.body
+    try {
+        const [user] = await db.promise().query('SELECT * FROM usuarios WHERE usuario_id = ?', [usuario])
+        if(!user[0]) return res.status(400).json({message: 'Usuario no existe'})
+        db.query('INSERT INTO cuenta (usuario, numero, saldo, tipo) values (?,?,?,?)',
+            [usuario, numero, saldo, tipo], 
+            (err, result) => {
+                if (err) {
+                    console.log(err.message)
+                    return res.status(400).json({message: 'Error al crear una cuenta'})
+                }
+                return res.status(200).json({message: "Cuenta creada", cuenta: result.insertId})
+            }
+        )
+    } catch (error) {
+        console.error('ERROR al crear cuenta')
+        return res.status(500).json({message: 'Error al crear cuenta'})
     }
 })
